3c35823d8eb3f669f60ec502b446cb82c14ef804
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-09-10 21:09:25 +0700

    Create install-for-bfm.sh

---
+++ install-for-bfm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -o ~/bfm-settings.ini https://raw.githubusercontent.com/taamarin/box_for_magisk/master/box/settings.ini
+su -c "mv ~/bfm-settings.ini /data/adb/box/settings.ini"
+su -c "sed -i 's/^ignore_out_list=.*/ignore_out_list=( \"wlan+\" )/' /data/adb/box/settings.ini"
+su -c "sed -i 's/^packages_list=.*/packages_list=( \"com.termux\" )/' /data/adb/box/settings.ini"
+curl -o ~/bfm-clash-config.yaml https://github.com/piscesCat/Psiphon/raw/khaiphan/bfm-clash-config.yaml
+su -c "mv ~/bfm-clash-config.yaml /data/adb/box/clash/config.yaml"
+su -c "/data/adb/box/scripts/box.service restart"
