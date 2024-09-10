868cebd3ee07655497d6a00ab98dd32b9e841713
piscesCat <135218537+piscesCat@users.noreply.github.com>
2024-09-10 16:27:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,12 @@ mkdir -p ~/psiphon
 tar -xzvf freedata.tar.gz -C ~/psiphon --strip-components=1 > /dev/null 2>&1
 
 chmod a+x ~/psiphon/psiphon-tun
-chmod a+x ~/psiphon/psiphon-login
+chmod a+x ~/psiphon/psiphon-tun-auto-boot.sh
 chmod a+x ~/psiphon/psiphon-tunnel-core
+
+mkdir -p ~/.termux/boot/
+
+cp -f ~/psiphon/psiphon-tun-auto-boot.sh ~/.termux/boot/
 
 rm freedata.tar.gz
 
